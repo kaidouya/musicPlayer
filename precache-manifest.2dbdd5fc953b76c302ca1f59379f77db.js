551acb1afdfada8915f32338e285e0a37fa37d32
kaidouya <kaidouya0527@gmail.com>
2019-12-11 14:57:41 +0800

    Updates

---
+++ precache-manifest.2dbdd5fc953b76c302ca1f59379f77db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b42bc01a0b9e8451f91e35956c9fec",
+    "url": "/musicPlayer/index.html"
+  },
+  {
+    "revision": "6a7a9e89a8066079d879",
+    "url": "/musicPlayer/static/css/main.6e401fc2.chunk.css"
+  },
+  {
+    "revision": "d101b1319f64dbd41ce2",
+    "url": "/musicPlayer/static/js/2.cb8c853c.chunk.js"
+  },
+  {
+    "revision": "6a7a9e89a8066079d879",
+    "url": "/musicPlayer/static/js/main.ef3c55a5.chunk.js"
+  },
+  {
+    "revision": "c1a6ccc650c7716b0d52",
+    "url": "/musicPlayer/static/js/runtime~main.06fa961b.js"
+  }
+]);
